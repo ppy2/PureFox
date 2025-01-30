7120e8f3c6414fa2da7e5425fdc17c6ea2fdb772
Pavel Pogodin <pavel.pogodin@gmail.com>
2025-01-30 18:15:55 +0300

    Вылечил вывод на консоль

--- ext_tree/board/luckfox/scripts/post-build.sh
+++ ext_tree/board/luckfox/scripts/post-build.sh
@@ -9,5 +9,5 @@ rm -f -r $TARGET_DIR/etc/alsa
 #rm -f -r $TARGET_DIR/var/db
 echo "uprclautostart = 1" > $TARGET_DIR/etc/upmpdcli.conf
 echo "friendlyname = PureOS" >> $TARGET_DIR/etc/upmpdcli.conf
-sed -i "s/console::respawn/#console::respawn/g" $TARGET_DIR/etc/inittab
+#sed -i "s/console::respawn/#console::respawn/g" $TARGET_DIR/etc/inittab
 
