4518b9ccbdc98cb9aea75eb07c7b14161ffcebfa
root <pavel.pogodin@gmail.com>
2025-02-10 10:44:54 +0300

    косметика

--- ext_tree/board/luckfox/rootfs_overlay/opt/update.sh
+++ ext_tree/board/luckfox/rootfs_overlay/opt/update.sh
@@ -10,20 +10,20 @@ sshpass -p 'luckfox'  rsync -av --size-only \
 --exclude=/etc/asound.conf \
 --exclude=/usr/aprenderer/config.dat \
 --exclude=/usr/aplayer/config.dat \
-luckfox@luckfox.puredsd.ru::luckfox / 
+luckfox@luckfox.puredsd.ru::luckfox / || exit 1
 
 
-flash_erase /dev/mtd0 0 0 || exit
-mtd_debug write /dev/mtd0 0 262144 /data/env.img || exit
+flash_erase /dev/mtd0 0 0 || exit 1
+mtd_debug write /dev/mtd0 0 262144 /data/env.img || exit 1
 sleep 2
-flash_erase /dev/mtd1 0 0 || exit
-mtd_debug write /dev/mtd1 0 262144 /data/idblock.img || exit
+flash_erase /dev/mtd1 0 0 || exit 1
+mtd_debug write /dev/mtd1 0 262144 /data/idblock.img || exit 1
 sleep 2
-flash_erase /dev/mtd2 0 0 || exit
-mtd_debug write /dev/mtd2 0 524288 /data/uboot.img || exit
+flash_erase /dev/mtd2 0 0 || exit 1
+mtd_debug write /dev/mtd2 0 524288 /data/uboot.img || exit 1
 sleep 2
-flash_erase /dev/mtd3 0 0 || exit
-mtd_debug write /dev/mtd3 0 4194304 /data/boot.img || exit
+flash_erase /dev/mtd3 0 0 || exit 1
+mtd_debug write /dev/mtd3 0 4194304 /data/boot.img || exit 1
 sleep 2
 
 
