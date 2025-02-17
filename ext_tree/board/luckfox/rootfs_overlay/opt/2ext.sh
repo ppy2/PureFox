e7f4f99989e31c1515672effb6f8330c12d31ca5
root <pavel.pogodin@gmail.com>
2025-02-17 10:47:12 +0300

    пофиксил скрипт переключения PLL/EXT

--- ext_tree/board/luckfox/rootfs_overlay/opt/2ext.sh
+++ ext_tree/board/luckfox/rootfs_overlay/opt/2ext.sh
@@ -4,7 +4,7 @@ sed -i 's/007c003c/007с001с/' /etc/init.d/S94ioi2s
 sed -i 's/MODE=pll/MODE=ext/' /etc/i2s.conf
 
 flash_erase /dev/mtd3 0 0
-mtd_debug write /dev/mtd3 0 4194304 /data/ext_boot.img
+mtd_debug write /dev/mtd3 0 4194304 /data/boot/ext_boot.img
 
 sync
 #reboot -f
--- ext_tree/board/luckfox/rootfs_overlay/opt/2pll.sh
+++ ext_tree/board/luckfox/rootfs_overlay/opt/2pll.sh
@@ -2,9 +2,11 @@
 
 sed -i 's/007c001c/007с003с/' /etc/init.d/S94ioi2s
 sed -i 's/MODE=ext/MODE=pll/' /etc/i2s.conf
+sed -i 's/MCLK=512/MCLK=1024/' /etc/i2s.conf
+
 
 flash_erase /dev/mtd3 0 0
-mtd_debug write /dev/mtd3 0 4194304 /data/pll_boot.img
+mtd_debug write /dev/mtd3 0 4194304 /data/boot/pll_boot.img
 
 sync
 #reboot -f
