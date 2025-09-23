14b587d0d78f3c15ed2f5f550bae9f03524372f8
root <pavel.pogodin@gmail.com>
2025-09-23 18:55:29 +0300

    fix volume qobuz-connect for default output

--- ext_tree/board/luckfox/rootfs_overlay/opt/2_std.sh
+++ ext_tree/board/luckfox/rootfs_overlay/opt/2_std.sh
@@ -5,8 +5,13 @@ ln -s /etc/asound.std /etc/asound.conf
 sed -i 's/^SUBMODE=.*$/SUBMODE=std/' /etc/i2s.conf
 echo I2S > /etc/output
 sync
+
+# Рестарт аудио сервисов
 if ls /etc/init.d/S95* >/dev/null 2>&1; then
     /etc/init.d/S95* restart
 else
     echo "Service S95* not found, skipping restart"
 fi
+
+/etc/init.d/S99statusmonitor restart &
+exit
