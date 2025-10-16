d694922a561a9da29636684f2e09095781edcbde
root <pavel.pogodin@gmail.com>
2025-10-16 10:14:01 +0300

    restart update.sh

--- ext_tree/board/luckfox/rootfs_overlay/opt/update.sh
+++ ext_tree/board/luckfox/rootfs_overlay/opt/update.sh
@@ -1,6 +1,41 @@
 #!/bin/sh
 
+SCRIPT_PATH="/opt/update.sh"
+SCRIPT_NEW="/tmp/update.sh.new"
+UPDATE_SERVER="luckfox@luckfox.puredsd.ru::luckfox_ultra/opt/update.sh"
+
+# Check if script update is needed (only on first run, not on restart)
+if [ "$UPDATE_SELF_DONE" != "1" ]; then
+    echo "Checking for update.sh updates..."
+
+    # Download new version of update.sh to temp location
+    sshpass -p 'luckfox' rsync -av "$UPDATE_SERVER" "$SCRIPT_NEW" 2>/dev/null
+
+    if [ $? -eq 0 ] && [ -f "$SCRIPT_NEW" ]; then
+        # Compare checksums
+        OLD_MD5=$(md5sum "$SCRIPT_PATH" | awk '{print $1}')
+        NEW_MD5=$(md5sum "$SCRIPT_NEW" | awk '{print $1}')
+
+        if [ "$OLD_MD5" != "$NEW_MD5" ]; then
+            echo "New version of update.sh found, updating..."
+            chmod +x "$SCRIPT_NEW"
+            cp "$SCRIPT_NEW" "$SCRIPT_PATH"
+            rm -f "$SCRIPT_NEW"
+
+            echo "Restarting with new update.sh..."
+            export UPDATE_SELF_DONE=1
+            exec "$SCRIPT_PATH" "$@"
+        else
+            echo "update.sh is up to date"
+            rm -f "$SCRIPT_NEW"
+        fi
+    else
+        echo "Could not check for updates, continuing..."
+    fi
+fi
+
 killall -9 status_monitor
+/etc/init.d/S95* stop
 
 sshpass -p 'luckfox' rsync -acv --delete --one-file-system \
 --exclude=.git \
