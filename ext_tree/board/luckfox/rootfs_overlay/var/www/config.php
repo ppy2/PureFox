5a5be495dfdb4946f44b7bc25e9e67e02cc7fa1f
root <pavel.pogodin@gmail.com>
2025-10-22 15:40:23 +0300

    Sync from master

--- ext_tree/board/luckfox/rootfs_overlay/var/www/config.php
+++ ext_tree/board/luckfox/rootfs_overlay/var/www/config.php
@@ -1,6 +1,6 @@
 <?php
 // System version
-define('VERSION', '1.5.2');
+define('VERSION', '1.5.3');
 ?>
 
 
--- ext_tree/package/librespot/librespot.mk
+++ ext_tree/package/librespot/librespot.mk
@@ -15,4 +15,10 @@ LIBRESPOT_DEPENDENCIES += host-rust-bindgen
 
 LIBRESPOT_DEPENDENCIES = alsa-lib  host-rust-bindgen host-pkgconf
 
+define LIBRESPOT_FIX_CPAL_CHECKSUM
+	sed -i 's/"src\/host\/alsa\/mod.rs":"8e7cc24a805b4729d43e01c1f0a7d315b1cb7b80fd97e394326a3c06cbf0eea9"/"src\/host\/alsa\/mod.rs":"b88a2b1d5e49dd4fecc158e11dbc484bbd6b554a8347dfdfd65a170bd2d46a57"/g' \
+		$(@D)/VENDOR/cpal/.cargo-checksum.json
+endef
+LIBRESPOT_POST_PATCH_HOOKS += LIBRESPOT_FIX_CPAL_CHECKSUM
+
 $(eval $(cargo-package))
